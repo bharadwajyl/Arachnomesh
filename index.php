6ee0edf08802e93d94c9e11bdb5a6d019b053ac4
Mahesh <100456710+bharadwajyl@users.noreply.github.com>
2022-07-13 01:06:49 +0000

    Update index.php

--- index.php
+++ index.php
@@ -22,6 +22,7 @@ class Arachnomesh{
         $array = array("ghi@hotmail.com", "def@yahoo.com", "ghi@gmail.com", "abc@channelier.com", 
         "abc@hotmail.com", "def@hotmail.com", "abc@gmail.com", "abc@yahoo.com", "def@channelier.com",
         "jkl@hotmail.com", "ghi@yahoo.com", "def@gmail.com" );
+        sort($array);
         $gmail = array();
         $hotmail = array();
         $yahoo = array();
